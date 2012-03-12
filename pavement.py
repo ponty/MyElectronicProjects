3fd526dc4650cabcc612802c5d5606dd02c4e07a
ponty <ponty@home>
2012-03-12 16:17:12 +0100

    config changed

--- pavement.py
+++ pavement.py
@@ -1,22 +1,16 @@
 from easyprocess import Proc
 from paver.easy import *
 
-try:
-    # Optional tasks, only needed for development
-    import paver.doctools
-    import paver.virtual
-    import paver.misctasks
-    from paved import *
-    from paved.dist import *
-    from paved.util import *
-    from paved.docs import *
-    from paved.pycheck import *
-    from sphinxcontrib import paverutils
-    ALL_TASKS_LOADED = True
-except ImportError, e:
-    info("some tasks could not not be imported.")
-    debug(str(e))
-    ALL_TASKS_LOADED = False
+import paver.doctools
+import paver.virtual
+import paver.misctasks
+from paved import *
+from paved.dist import *
+from paved.util import *
+from paved.docs import *
+from paved.pycheck import *
+from paved.pkg import *
+from sphinxcontrib import paverutils
 
 options(
     sphinx=Bunch(
@@ -30,36 +24,46 @@ options(
     )
 
 
-if ALL_TASKS_LOADED:
-    
-    options.paved.clean.patterns += ['*.pickle',
-                                     '*.doctree',
-                                     '*.gz' ,
-                                     'nosetests.xml',
-                                     'sloccount.sc',
-                                     '*.pdf', '*.tex',
-                                     '*.png',
-                                     '*.b#*', '*.s#*','*.pro', # eagle
-                                     '*.hex',
-                                     ]
-    
-    options.paved.dist.manifest.include.remove('distribute_setup.py')
-    
-    @task
-    @needs('cog', 'html', 'pdf')
-    def alltest():
-        'all tasks to check'
-        pass
-    
-    @task
-    @needs('sphinxcontrib.paverutils.html')
-    def html():
-        pass
+options.paved.clean.rmdirs +=   ['.tox',
+                                 'dist',
+                                 'build' ,
+                                 ]
+options.paved.clean.patterns += ['*.pickle',
+                                 '*.doctree',
+                                 '*.gz' ,
+                                 'nosetests.xml',
+                                 'sloccount.sc',
+                                 '*.pdf', '*.tex',
+                                 '*.png',
+                                 '*.b#*', '*.s#*','*.pro', # eagle
+                                 '*.hex',
+                                 '*.zip',   
+                                 'distribute_setup.py',
+                                 ]
 
-    @task
-    @needs('sphinxcontrib.paverutils.pdf')
-    def pdf():
-        fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
-        d=path('docs/_build/html')
-        d.makedirs()
-        fpdf.copy(d)
+options.paved.dist.manifest.include.remove('distribute_setup.py')
+options.paved.dist.manifest.include.remove('paver-minilib.zip')
+
+@task
+@needs(
+#           'clean',
+       'cog', 
+       'html', 
+       'pdf',
+       )
+def alltest():
+    'all tasks to check'
+    pass
+
+@task
+@needs('sphinxcontrib.paverutils.html')
+def html():
+    pass
+
+@task
+@needs('sphinxcontrib.paverutils.pdf')
+def pdf():
+    fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
+    d=path('docs/_build/html')
+    d.makedirs()
+    fpdf.copy(d)
