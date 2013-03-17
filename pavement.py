b00e23531dd671bb348f6b6f4a0ca1ad4f09a6fd
ponty <ponty@home>
2013-03-17 20:01:02 +0100

    simplified pavement tasks

--- pavement.py
+++ pavement.py
@@ -10,17 +10,16 @@ from paved.util import *
 from paved.docs import *
 from paved.pycheck import *
 from paved.pkg import *
-from sphinxcontrib import paverutils
 
 options(
     sphinx=Bunch(
         docroot='docs',
         builddir="_build",
     ),
-    pdf=Bunch(
-        builddir='_build',
-        builder='latex',
-    ),
+#    pdf=Bunch(
+#        builddir='_build',
+#        builder='latex',
+#    ),
 )
 
 
@@ -64,18 +63,3 @@ options.paved.dist.manifest.include.remove('paver-minilib.zip')
 def alltest():
     'all tasks to check'
     pass
-
-
-@task
-@needs('sphinxcontrib.paverutils.html')
-def html():
-    pass
-
-
-@task
-@needs('sphinxcontrib.paverutils.pdf')
-def pdf():
-    fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
-    d = path('docs/_build/html')
-    d.makedirs()
-    fpdf.copy(d)
